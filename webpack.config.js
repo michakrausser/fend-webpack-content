336b3b73ed176eacbb5c29616fcc14db3f9c4a3f
Micha Kraußer <michak@synphonyte.com>
2021-01-31 20:38:29 -0600

    prepare entry and output point, set up loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,23 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './src/client/index.js',
+
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: '/\\.(png|jpe?g|gif)$/i',
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ]
+            }
+        ]
+    }
 }
+
